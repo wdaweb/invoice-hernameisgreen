5165e184b3a983c6cc41b75aa326365afdf013c5
sadie <hernameisgreen@gmail.com>
2020-11-18 14:35:57 +0800

    完成對單張發票的對獎做法

--- award.php
+++ award.php
@@ -26,6 +26,53 @@ $period=ceil(explode('-', $date)[1]/2);
 
 $awards=$pdo->query("select * from award_numbers where year='$year' && period='$period'")->fetchAll();
 
-echo "<pre>";
+/* echo "<pre>";
 print_r($awards);
-echo"</pre>";
+echo"</pre>"; */
+/* $awards is an array while $number is a string */
+foreach($awards as $award){
+    switch($award['type']){
+        case 1:
+            /* 特別號 */
+            if($award['number']==$number){
+                echo "<br>號碼=".$number."<br>";
+                echo "congrats! you've won the 特別獎!";
+            }else{
+                echo "aww nope<br>";
+            }
+           
+        break;
+        case 2:
+            /* 1st prize */
+            if($award['number']==$number){
+                echo "<br>號碼=".$number."<br>";
+                echo "congrats! you've won the 特獎!";
+            }
+        break;
+        case 3:
+            $res=0;
+            for($i=5;$i>=0;$i--){
+                $target=mb_substr($award['number'], $i,(8-$i),'utf8' );
+                $mynumber=mb_substr($number, $i,(8-$i),'utf8' );
+            
+            if($target==$mynumber){
+         /*        echo "<br>號碼=".$number."<br>";
+                echo "congrats! you've won {$awardStr[$i]}prize<br>"; */
+                $res=$i;
+            }else{
+            break;
+            //continue
+
+            }
+        }
+        echo "<br>號碼=".$number."<br>";
+        echo "congrats! you've won $res prize<br>";
+        break;
+        case 4:
+            if($award['number']==mb_substr($number,5,3,'utf8')){
+                echo "<br>號碼=".$number."<br>";
+                echo "中了增開六獎";
+            }
+        break;
+    }
+}
--- base.php
+++ base.php
@@ -4,3 +4,5 @@ $pdo=new pdo($dsn, 'root', '');
 
 date_default_timezone_set("Asia/Taipei");
 session_start();
+
+$awardStr=['頭','二','三','四','五','六'];
