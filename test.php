392d3a3316d989796500c1a6840c7fb51297f5cb
sadie <hernameisgreen@gmail.com>
2020-11-20 13:12:12 +0800

    取得單一資料的自訂函式

--- test.php
+++ test.php
@@ -7,19 +7,56 @@ include "base.php";
 //"select * from invoices where id='9'";
 //$row=$pdo->query("select * from invoices where id='9'")->fetch();
 
-$row= find('invoices', "id='16'");
-echo $row['code'];
-echo $row['number'];
+echo implode("&&", ['欄位1'=>'值1','欄位2'=>'值2', 'id' => '9']);
+echo "<hr>";
+
+$array=['欄位1'=>'值1','欄位2'=>'值2', 'id' => '9'];
+/* foreach($array as $key => $value){
+    echo $key. "= '".$value."'&&";
+} */
+
+//利用一個暫時的陣列來存放語句片段
+foreach($array as $key => $value){
+    /* $tmp[]="`".$key."`='".$value."'"; */
+
+    $tmp[]=sprintf("`%s` = '%s'",$key, $value);
+}
+print_r($tmp);
+echo "<br>";
+
+//使用implode把暫時陣列中的語句片段串成SQL會使用到的語句
+echo implode ("&&", $tmp);
 
 
-function find($table, $def){
-    global $pdo;
+$row= find('invoices', '21');
+echo $row['code'].$row['number'];
 
-    /* 使用全域變數pdo */
+$row= find('invoices', ['code'=>'GD', 'number'=>'01589816']);
+echo $row['code'].$row['number'];
 
-    $sql="select * from $table where $def";
+
+
+/*取得單一資料的自訂函式*/
+
+function find($table, $id){
+global $pdo;
+/* 使用全域變數pdo */
+
+$sql="select * from $table where";
+
+
+if(is_array($id)){
+    foreach($id as $key => $value){
+        $tmp[]=sprintf("`%s` = '%s'",$key, $value);
+
+    } $sql=$sql.implode("&&", $tmp);
+
+}else{
+    $sql=$sql."id='$id'";
+    /* 不是真的id,只是名字而已 */
+}
+    
     $row= $pdo->query($sql)->fetch();
     return $row;
 }
-
 ?>
